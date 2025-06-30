96c110905e6343608f7b96c2e7325e0edd0bdca9
DeathG7n <christariccykid55@gmail.com>
2025-06-30 15:53:34 +0100

    changes

--- tele.js
+++ tele.js
@@ -20,7 +20,12 @@ app.get("/", async(req, res)=>{
 })
 
 app.listen(3000,()=>{
-  console.log("Server is running")
+  console.log("Server is running") 
+  setInterval(()=>{
+    assets.forEach((asset)=>{ 
+      getSignal(asset) 
+    })
+  }, 10000) 
 })
 
 const assets = [
@@ -81,63 +86,63 @@ function getTicksRequest(symbol, count, timeframe){
 
 const getSignal = async (asset) => {
   try{
-    const period1 = getTicksRequest(asset?.symbol, 10000000000000000000 , getTimeFrame(1, "mins"))
-    const period15 = getTicksRequest(asset?.symbol, 10000000000000000000 , getTimeFrame(15, "mins"))
+    const period5 = getTicksRequest(asset?.symbol, 10000000000000000000 , getTimeFrame(5, "mins"))
+    const period60 = getTicksRequest(asset?.symbol, 10000000000000000000 , getTimeFrame(1, "hrs"))
 
-    const candles1 = await api.ticksHistory(period1);
-    const candles15 = await api.ticksHistory(period15);
+    const candles5 = await api.ticksHistory(period5);
+    const candles60 = await api.ticksHistory(period60);
 
-    const closePrices1 = candles1?.candles?.map(i => {return i?.close})
-    const openPrices1 = candles1?.candles?.map(i => {return i?.open})
-    const closePrices15 = candles15?.candles?.map(i => {return i?.close})
-    const openPrices15 = candles15?.candles?.map(i => {return i?.open})
+    const closePrices5 = candles5?.candles?.map(i => {return i?.close})
+    const openPrices5 = candles5?.candles?.map(i => {return i?.open})
+    const closePrices60 = candles60?.candles?.map(i => {return i?.close})
+    const openPrices60 = candles60?.candles?.map(i => {return i?.open})
 
-    const len = closePrices1?.length;
+    const len = closePrices5?.length;
     const currIndex = len - 1;
     const prevIndex = len - 2;
     const secondIndex = len - 3
     const thirdIndex = len - 4
 
-    const ema14 = calculateEMA(closePrices1, 14);
-    const ema21 = calculateEMA(closePrices1, 21);
-    const ema14_15 = calculateEMA(closePrices15, 14);
-    const ema21_15 = calculateEMA(closePrices15, 21);
+    const ema14 = calculateEMA(closePrices5, 14);
+    const ema21 = calculateEMA(closePrices5, 21);
+    const ema14_60 = calculateEMA(closePrices60, 14);
+    const ema21_60 = calculateEMA(closePrices60, 21);
 
     const ema14Now = ema14[currIndex];
     const ema21Now = ema21[currIndex];
-    const ema14_15Now = ema14_15[currIndex];
-    const ema21_15Now = ema21_15[currIndex];
+    const ema14_60Now = ema14_60[currIndex];
+    const ema21_60Now = ema21_60[currIndex];
 
     const upTrend = ema14Now > ema21Now
     const downTrend = ema14Now < ema21Now
-    const upTrend15 = ema14_15Now > ema21_15Now
-    const downTrend15 = ema14_15Now < ema21_15Now
+    const upTrend15 = ema14_60Now > ema21_60Now
+    const downTrend15 = ema14_60Now < ema21_60Now
 
-    const higherBullSignal = upTrend15 && bearish15(secondIndex) && bullish15(prevIndex)
-    const higherBearSignal = downTrend15 && bullish15(secondIndex) && bearish15(prevIndex)
-    const lowerBullSignal = upTrend && (bearish1(secondIndex) && bullish1(prevIndex) || bearish1(thirdIndex) && bullish1(secondIndex) && bullish1(prevIndexIndex))
-    const lowerBearSignal = downTrend && (bullish1(secondIndex) && bearish1(prevIndex) || bullish1(thirdIndex) && bearish1(secondIndex) && bearish1(prevIndex))
+    const higherBullSignal = upTrend15 && bearish60(secondIndex) && bullish60(prevIndex)
+    const higherBearSignal = downTrend15 && bullish60(secondIndex) && bearish60(prevIndex)
+    const lowerBullSignal = upTrend && (bearish5(secondIndex) && bullish5(prevIndex) || bearish5(thirdIndex) && bullish5(secondIndex) && bullish5(prevIndexIndex))
+    const lowerBearSignal = downTrend && (bullish5(secondIndex) && bearish5(prevIndex) || bullish5(thirdIndex) && bearish5(secondIndex) && bearish5(prevIndex))
 
     const buySignal = higherBullSignal && lowerBullSignal
     const sellSignal = higherBearSignal && lowerBearSignal
 
-    function bearish1(candle){
-      return openPrices1[candle] > closePrices1[candle]
+    function bearish5(candle){
+      return openPrices5[candle] > closePrices5[candle]
     }
-    function bullish1(candle){
-      return closePrices1[candle] > openPrices1[candle]
+    function bullish5(candle){
+      return closePrices5[candle] > openPrices5[candle]
     }
 
-    function bearish15(candle){
-      return openPrices15[candle] > closePrices15[candle]
+    function bearish60(candle){
+      return openPrices60[candle] > closePrices60[candle]
     }
-    function bullish15(candle){
-      return closePrices15[candle] > openPrices15[candle]
+    function bullish60(candle){
+      return closePrices60[candle] > openPrices60[candle]
     }
-    currentCandles[asset.count] = closePrices1[currIndex]
+    currentCandles[asset.count] = closePrices5[currIndex]
 
-    if(previousCandles[asset.count] !== closePrices1[prevIndex]){
-      previousCandles[asset.count] = closePrices1[prevIndex]
+    if(previousCandles[asset.count] !== closePrices5[prevIndex]){
+      previousCandles[asset.count] = closePrices5[prevIndex]
       if(buySignal){
         sendMessage(`${asset?.name} is bullish`)
         console.log(`${asset?.name} is bullish`)
