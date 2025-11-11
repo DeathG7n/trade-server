36ff45655924e9587539f26ed8d9505092a3d18e
DeathG7n <christariccykid55@gmail.com>
2025-11-11 10:14:52 +0100

    changes

--- trade.js
+++ trade.js
@@ -122,7 +122,7 @@ function buyMultiplier(direction, sym, stake) {
       currency: "USD",
       symbol: sym,
       multiplier: 750,
-      limit_order: { stop_loss: stake / 5, take_profit: stake },
+      limit_order: { stop_loss: stake / 5, take_profit: stake / 2},
     },
   });
 }
