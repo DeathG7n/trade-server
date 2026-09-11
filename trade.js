f5aa1148a4881a4b692ae86812868b1886b9cf92
DeathG7n <christariccykid55@gmail.com>
2026-09-11 10:02:55 +0100

    fix: add candleDistance import to enhance trading calculations

--- trade.js
+++ trade.js
@@ -6,7 +6,7 @@ import axios from "axios";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 
-import { bearish, bullish, calculateATR, crossedEma } from "./util.js";
+import { bearish, bullish, calculateATR, crossedEma, candleDistance } from "./util.js";
 
 dotenv.config();
 
